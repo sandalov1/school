1f45407cfdccb98d702b6d06943ad81373e618b5
BWKenobi <boriszhuravlev@gmail.com>
2020-04-01 15:47:51 +0300

    First commit

--- school/settings.py
+++ school/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n@!^5l_pph!@1eyc+^wx0rm7%x&jpibfwy1b6zmvy075y2fua+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sandalov1.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
